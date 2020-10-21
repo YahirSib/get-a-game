bed76c4b25371e8ac74363e4911414c82edb3676
YahirSib <67615899+YahirSib@users.noreply.github.com>
2020-10-20 23:32:57 -0600

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <link rel="stylesheet" href="CSS/dashboard.css">
+        <link rel="stylesheet" href="CSS/menu.css">
+        <link rel="stylesheet" type="text/css" href="css/basic.css">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400;700&display=swap" rel="stylesheet">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function(){
+	        $('.hamburger').click(function() {
+		    $('.hamburger').toggleClass('is-active');
+	         });
+            });
+        </script>
+    </head>
+<body>
+	<div id="prev">
+        <div>
+            <img class="ult" src="media/1.png"id="1">
+            <img class="ult" src="media/2.png"id="2">
+            <img class="ult" src="media/3.png"id="3">
+        </div>
+        <div class="Coso">
+            <div class="r5">
+                <div class="r4">
+                    <div class="r3">
+                        <div class="r2">
+                            <div class="r1"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tapar">
+            <div></div><div></div><div></div><div></div><div></div>
+        </div>
+    </div>
+    <div id="name">
+        <span></span>
+        <span></span>
+        <span></span>-
+        <span></span>-
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+<div id="body">
+	<div class="header">
+<input type="checkbox" id="btn-menu">
+        <label for="btn-menu">
+        <div class="hamburger">
+	            <div class="_layer -top"></div>
+	            <div class="_layer -mid"></div>
+	            <div class="_layer -bottom"></div>
+            </div>
+		</label>
+    <nav id="menu">
+        <ul>
+            <a href=""><li>Inicio</li></a>
+            <a href=""><li>Sobre Nosotros</li></a>
+            <a href=""><li>Catalogo</li></a>
+        	<a href="vis.loginUser.php"><li>Iniciar Sesion</li></a>
+        	<a href="vis.registro.php"><li>Registrarse</li></a>
+        </ul>
+    </nav>
+</div>
+<header>
+        <div class="textos">
+            <h1 class="titulo">Get - A - Game</h1>
+            <h3 class="subtitulo">Videojuegos son vida, Videojuegos son amor</h3>
+            <a href="#" class="boton">Contactanos</a>
+        </div>
+    </header>
+    <article>
+    <section class="grid">
+        <div class="textos-hero">
+            <h1 class="weight-600">
+                Sobre Nosotros
+            </h1><br>
+            <p class="parrafo">
+                Como empresa se busca hacer que los productos no se vean como un desperdicio de dinero, sino que el cliente lo vea como una oportunidad de que su vida cotidiana no quede ahí, al ofrecer un productos como los videojuegos queremos darle al cliente ese ambiente externo a la realidad envolviéndolo en alguno de los grandes títulos de este gran número y hacer la experiencia del cliente una experiencia única y de gran satisfacción.
+            </p><br>
+            <a href="" class="cta">Conoce más</a>
+        </div>
+        <div class="img-hero">
+            <img src="IMG/logo.png" class ="img">
+        </div> 
+    </section>
+    </article>
+    <article>
+    <section class="grid2">
+    	<div class="img-hero2">
+            <img src="IMG/logo2.png" class ="img2">
+       	</div> 
+        <div class="textos-hero1">
+            <h1 class="weight-600X2">
+                Visión
+            </h1><br>
+            <p class="parrafo1">
+                Somos una empresa orientada a la venta y ofrecimiento de servicios relacionados con el negocio de los videojuegos, en el rubro de accesorios, consolas, títulos, y mucho más. Nuestra principal misión comprende atender y satisfacer las necesidades de la juventud, adultos e infantes que disfrutan de los títulos de juegos de video que también nosotros apreciamos, de modo que esto nos permita ofrecerle los mejores productos al mejor precio.
+            </p><br>
+        </div>
+        <div class="textos-hero2">
+            <h1 class="weight-600X3">
+                Misión
+            </h1><br>
+            <p class="parrafo2">
+                Nuestra visión es ofrecer los mejores productos y servicios para video jugadores con la mejor calidad posible. Esta visión tiene como objetivo lograr la calidad y excelencia en el servicio aprovechando al máximo el talento del equipo; para convertirnos en líderes en términos de calidad de servicio y productos mediante habilidades profesionales.
+            </p><br>
+        </div>
+    </section>
+    </article>
+   <section>
+    <div class="label2">
+        <p class="texto2">Conoce los diferentes mundos</p>
+    </div>
+    <div class="contenedor">
+    <div class="carrusel">
+        <div class="imagenes-transformables" id="img1" >
+            <div class="fade">
+                <div class="border-text">
+                    Nintendo
+                </div>
+            </div>
+        </div>
+        <div class="imagenes-transformables" id="img2" >
+            <div class="fade">
+                <div class="border-text">
+                    Xbox
+                </div>
+            </div>
+        </div>
+        <div class="imagenes-transformables" id="img3" >
+            <div class="fade">
+                <div class="border-text">
+                    Playstation
+                </div>
+            </div>
+        </div> 
+        <div class="imagenes-transformables" id="img4" >
+            <div class="fade">
+                <div class="border-text">
+                    PC
+                </div>
+            </div>
+        </div>
+   </div>
+  </section>
+   <footer>
+       <div class="gridF">
+           <div class="textos-footer">
+               <h2 class ="subtitulo2"> Contactanos </h2>
+               <p class="text2">Derechos reservados 2020 | Get - A - Game</p>
+           </div>
+           <div class="footer-items1">
+               <a href="">
+                    <img src="IMG/face.png" height="70px" weight="70px">
+               </a>
+           </div>
+           <div class="footer-items2">
+                <a href="">
+                <img src="IMG/ig.png" height="70px" weight="70px">
+                </a>
+           </div>
+           <div class="footer-items3">
+                <a href="">
+                <img src="IMG/correo.png" height="80px" weight="80px">
+                </a>
+           </div>
+           <div class="footer-items4">
+                <a href="">
+                <img src="IMG/wha.png" height="80px" weight="80px">
+                </a>
+           </div>
+       </div>
+   </footer>
+ </div>
+</body>
+</html>
